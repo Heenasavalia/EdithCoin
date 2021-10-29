19a4a1fd53d1c336141ac8209e276019f33b2917
heenasavalia <heenasavaliya2704@gmail.com>
2021-10-29 16:16:51 +0530

    change spellings affilate too afiiliate

--- resources/views/client/affilates.blade.php
+++ resources/views/client/affilates.blade.php
@@ -13,8 +13,8 @@
       </div>
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">affilate</li>
+          <li class="breadcrumb-item"><a href="{{url('client/home')}}">Home</a></li>
+          <li class="breadcrumb-item active">Affiliate</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
